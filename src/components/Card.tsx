ed4361c2f397e95f7e3a0ba41eaea9e7acd0fb4c
Subodh-27 <sbhawt@gmail.com>
2024-02-02 16:46:30 +0530

    Project Completed with CSS

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+import "./Card.css";
+
 interface QuoteTypes {
     quote: string;
     author: string;
@@ -26,21 +28,26 @@ const Card = () => {
     }
 
     return (
-        <div className="container">
-            {
-                !loading 
-                ? ( <div className="loading-spinner">Loading</div> ) 
-                : <h2>Breaking Bad Quotes</h2>
-            }
+        <>
+            <div className="container">
+                {
+                    !loading 
+                    ? ( <div className="loading-spinner"></div> ) 
+                    : <h2>Breaking Bad Quotes</h2>
+                }
 
-            {
-                randomQuote.map((quote, index) => (
-                    <span key={index}>
-                        <p>{quote.quote}</p>
-                    </span>
-                ))
-            }
-        </div>
+                {
+                    randomQuote.map((quote, index) => (
+                        <span key={index}>
+                            <p>{ quote.quote }</p>
+                            <h4>- { quote.author }</h4>
+                        </span>
+                    ))
+                }
+            </div>
+
+            <button onClick={() => window.location.reload() }>Next</button>
+        </>
     )
 }
 
