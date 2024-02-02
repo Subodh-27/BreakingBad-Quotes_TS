7ad16203ef03782137136bd420fcaa8ac7a890df
Subodh-27 <sbhawt@gmail.com>
2024-02-02 16:13:21 +0530

    first commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+interface QuoteTypes {
+    quote: string;
+    author: string;
+}
+
+const Card = () => {
+    
+    const [randomQuote, setRandomQuote] = useState<QuoteTypes[]>([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        fetchQuotes()
+    }, [])
+
+    const fetchQuotes = async() => {
+        try {
+            const response = await fetch("https://api.breakingbadquotes.xyz/v1/quotes")
+            const quotes = await response.json();
+            setRandomQuote(quotes)
+            setLoading(true)
+        } catch {
+            console.error("Something went Wrong");
+        }
+    }
+
+    return (
+        <div className="container">
+            {
+                !loading 
+                ? ( <div className="loading-spinner">Loading</div> ) 
+                : <h2>Breaking Bad Quotes</h2>
+            }
+
+            {
+                randomQuote.map((quote, index) => (
+                    <span key={index}>
+                        <p>{quote.quote}</p>
+                    </span>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Card;
